77a5d182acdf62f55e601d67dfcd1c39352db302
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-03-26 05:00:30 +0530

    create new Student class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -19,7 +19,13 @@ public class MyMain {
         myBinarySearchTree.add(8);
         myBinarySearchTree.add(15);
         myBinarySearchTree.add(1);
+        System.out.println("Traverse the tree in PreOrder :");
+        traversePreOrder(myBinarySearchTree.getRoot());
+        System.out.println(
 
+        );
+        System.out.println("Traverse the tree in PostOrder :");
+        traversePostOrder(myBinarySearchTree.getRoot());
     }
 
     public static void traversePreOrder(TreeNode<Integer> currentNode) {
