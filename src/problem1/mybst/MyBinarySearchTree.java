abdf79d3e259c9fc4f7722a36049593fa648077b
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-03-25 12:48:11 +0530

    create new addRecursive(TreeNode<E> node) of returning type TreeNode<E>

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -10,11 +10,17 @@ import problem1.node.TreeNode;
 
 // to implement BinarySearchTree
 public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
+    private TreeNode<E> root;
+    private int numberOfLeftChild = 0;
+
     @Override
     public boolean add(E data) {
         return false;
     }
 
-    private TreeNode<E> root;
-    private int numberOfLeftChild = 0;
+    private TreeNode<E> addRecursive(TreeNode<E> node) {
+
+        return node;
+    }
+
 }
