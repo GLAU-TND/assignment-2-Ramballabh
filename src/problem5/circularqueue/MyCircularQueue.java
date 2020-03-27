5d41ab37a0aa5baa5696c497056c0ae2276a1420
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-03-27 13:42:49 +0530

    define new private fields rear and size

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -6,10 +6,14 @@
  */
 package problem5.circularqueue;
 
+import problem3.node.Node;
 import problem5.adt.CircularQueueADT;
 
 //to implement circular queue
 public class MyCircularQueue<E> implements CircularQueueADT<E> {
+    private Node<E> rear;
+    private int size;
+
     @Override
     public void enqueue(E data) {
 
