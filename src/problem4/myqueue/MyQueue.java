74b725bd1eb8cad9ec6a2d6c1018344e2108a596
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-03-28 12:37:40 +0530

    define constructor and getter and setters

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -39,5 +39,30 @@ public class MyQueue<E> implements MyQueueADT<E> {
     private static class Node<E> {
         E data;
         Node<E> next;
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 }
