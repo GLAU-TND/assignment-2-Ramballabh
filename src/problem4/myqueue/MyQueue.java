f4706fcbaa3fd30589e5d7708aba5a89b8730a36
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-03-28 13:07:03 +0530

    give definition to all methods

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -16,27 +16,41 @@ public class MyQueue<E> implements MyQueueADT<E> {
 
     @Override
     public void enqueue(E data) {
-
+        Node<E> node = new Node<>(data);
+        if (front == null) {
+            node.next = null;
+            front = node;
+            rear = node;
+        } else {
+            node.setNext(rear.getNext());
+            rear.setNext(node);
+            rear = node;
+        }
+        size++;
     }
 
     @Override
     public E dequeue() {
-        return null;
+        E data = front.getData();
+        front = front.getNext();
+        size--;
+        return data;
     }
 
     @Override
     public E peek() {
-        return null;
+        E data = front.getData();
+        return data;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
     private static class Node<E> {
